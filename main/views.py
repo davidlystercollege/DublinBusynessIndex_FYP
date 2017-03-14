bca4424c2f104885619ec2717fce0ce280dc335f
davidlyster <davidlyster@myclick.ie>
2017-03-14 13:35:12 +0000

    adding first line graph -12

--- main/views.py
+++ main/views.py
@@ -45,7 +45,7 @@ def home(request):
         tempBiz = BusynessIndex.objects.get(id = (sze-i))
         bizys1.append(tempBiz.busyness)
         
-        tmptym = (tempBiz.dateTaken).timestamp()
+        tmptym = (tempBiz.dateTaken).replace(tzinfo=timezone.utc).timestamp()
         
         times1.append(tmptym)
     ################################################
