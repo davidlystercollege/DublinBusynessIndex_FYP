573e7a4bfa4d32c8a1d4ff73fdb12bd37d5527f3
davidlyster <davidlyster@myclick.ie>
2017-03-17 12:19:14 +0000

    std stuff

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='main home'),
-    url(r'^test', views.testing, name='Testing Page'),
+    url(r'home', views.home, name='main home'),
+    url(r'test', views.testing, name='Testing Page'),
     url(r'busyness', views.mainBusyness, name='main busyness output'),
 ]
