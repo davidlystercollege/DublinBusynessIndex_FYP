66e88eabe01d88f77ff275d5d9350fa623d0092f
davidlyster <davidlyster@myclick.ie>
2017-03-17 10:33:08 +0000

    new tests

--- m50times/views.py
+++ m50times/views.py
@@ -184,7 +184,7 @@ def m50times(request):
         #return ans
         data=data + "Busyness = " +str(busyness)
         busyness=(busyness*100)
-        return busyness
+        return data
     
-    #return HttpResponse(getCurrTTBusyness())
-    return getCurrTTBusyness()
+    return HttpResponse(getCurrTTBusyness())
+    #return getCurrTTBusyness()
