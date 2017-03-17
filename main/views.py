5360753b2becdf1cb0bed8a3ac30d6db67ad3f91
davidlyster <davidlyster@myclick.ie>
2017-03-17 13:40:28 +0000

    std stuff

--- main/views.py
+++ main/views.py
@@ -49,7 +49,7 @@ def home(request):
     
     for i in range(13):
         indx = sze-(i-1)
-        tempBiz1 = allBusynessz.get(id = (sze-(i-15)))
+        tempBiz1 = allBusynessz.get(id = (sze-(i+15)))
         bizys1.append(tempBiz1.busyness)
         
         #tmptym = (tempBiz.dateTaken).time()
