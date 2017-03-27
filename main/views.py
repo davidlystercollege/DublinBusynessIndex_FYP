7fece89dc8c41f0cf0d5d423e727ceea6b9b139d
davidlyster <davidlyster@myclick.ie>
2017-03-27 17:31:38 +0100

    testing for failed m50 dataset get requests -2, seems to be working now

--- main/views.py
+++ main/views.py
@@ -536,8 +536,8 @@ def mainBusyness(request):
         noiseVal = noiseLevels.views.noiseLevels(request)
         createBusynessSub(request, nl_dso, noiseVal)
         
-        #m50Val = m50times.views.m50times(request)
-        m50Val = 19.34342113
+        m50Val = m50times.views.m50times(request)
+        #m50Val = 19.34342113
         createBusynessSub(request, m50_dso, m50Val)
         
         cpVal = carParks.views.carParks(request)
