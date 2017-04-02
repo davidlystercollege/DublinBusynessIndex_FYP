eee3708d9e9a27723b1c846281a3332cbd76d0ce
davidlyster <davidlyster@myclick.ie>
2017-04-03 00:58:33 +0100

    changing stuff in main

--- main/views.py
+++ main/views.py
@@ -535,7 +535,7 @@ def mainBusyness(request):
         
         noiseVal = noiseLevels.views.noiseLevels(request)
         #createBusynessSub(request, nl_dso, noiseVal)
-        
+        '''
         m50Val = m50times.views.m50times(request)
         #m50Val = 19.34342113
         #createBusynessSub(request, m50_dso, m50Val)
@@ -557,12 +557,13 @@ def mainBusyness(request):
         #ans=ans + "Busy: " + str(busynessIndex) + ", noise,m50,cp,bk := " + str(noiseVal) + ", " +str(m50Val)+",  "+str(bikeVal)
         #ans=ans + "Busy: " + str(busynessIndex) + ", noise,m50,cp,bk := " + str(noiseVal) + ", " +str(m50Val)+", "+str(cpVal)+", "+str(bikeVal)
         ans = ans+"<br>"+"Busyness: "+str(busynessIndex)
-        
+        '''
         '''busnisses = BusynessIndex.objects.all()
         for i in range(1, 10):
             ans = ans + "-" + str(i) + ": " + str(busnisses[i]) + "<br><br/>"
         '''
+        ans = 'y'
         return ans
 
     #test = dublinBikes.views.dubBikes(request)
-    return getBusynessValues()
+    return HttpResponse(getBusynessValues())
