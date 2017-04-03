fdde998912f27f5f7d4b733b241b23f7aa439b8b
davidlyster <davidlyster@myclick.ie>
2017-04-03 17:58:29 +0100

    trying to get days of datetimes

--- main/views.py
+++ main/views.py
@@ -103,8 +103,8 @@ def home(request):
             
         bizys3.append(tempBiz3.busyness)
         
-        if(tempBiz3.dateTaken.day == 0):
-            mondays.append(tempBiz3)
+        #if(tempBiz3.dateTaken.day == 0):
+         #   mondays.append(tempBiz3)
         
         a = datetime.datetime.strptime(str(tempBiz3.dateTaken), '%Y-%m-%d %H:%M:%S.%f+00:00').strftime('%s')
         d_in_ms = int(a)*1000
