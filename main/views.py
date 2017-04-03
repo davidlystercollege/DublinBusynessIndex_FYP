7f66a5215c121f744e108e9c8647de67b8c2a5e3
davidlyster <davidlyster@myclick.ie>
2017-04-03 17:45:16 +0100

    trying to get days of datetimes

--- main/views.py
+++ main/views.py
@@ -90,6 +90,7 @@ def home(request):
     ################################################
     
     ########### Line Graph 3 Data #####################
+    mondays = []
     for i in range(3000):
         indx = sze-(i-1)
         
@@ -100,6 +101,9 @@ def home(request):
         tempBiz3 = allBusynessz.get(id = (indx))
             
         bizys3.append(tempBiz3.busyness)
+        
+        if(tempBiz3.dateTaken.day == 0):
+            mondays.append(tempBiz3)
         
         a = datetime.datetime.strptime(str(tempBiz3.dateTaken), '%Y-%m-%d %H:%M:%S.%f+00:00').strftime('%s')
         d_in_ms = int(a)*1000
