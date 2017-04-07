16424d965ae46a4d778c4e2dcc236ada7db53403
davidlyster <davidlyster@myclick.ie>
2017-04-07 00:29:30 +0000

    changing partition size in dash -1

--- main/views.py
+++ main/views.py
@@ -595,21 +595,19 @@ def mainBusyness(request):
         noiseVal = noiseLevels.views.noiseLevels(request)
         createBusynessSub(request, nl_dso, noiseVal)
         
-        try:
-            m50Val = m50times.views.m50times(request)
-        except:
-            m50Val = 10.34342113
+        
+        m50Val = m50times.views.m50times(request)
         createBusynessSub(request, m50_dso, m50Val)
         
-        cpVal = carParks.views.carParks(request)
-        createBusynessSub(request, cp_dso, cpVal)
+        #cpVal = carParks.views.carParks(request)
+        #createBusynessSub(request, cp_dso, cpVal)
         
-        bikeVal = dublinBikes.views.dubBikes(request)
-        createBusynessSub(request, db_dso, bikeVal)
+        #bikeVal = dublinBikes.views.dubBikes(request)
+        #createBusynessSub(request, db_dso, bikeVal)
         
-        busynessIndex = ( (noiseVal*weigths[0]) + (bikeVal*weigths[1]) + (cpVal*weigths[2]) + (m50Val*weigths[3]) )
+        #busynessIndex = ( (noiseVal*weigths[0]) + (bikeVal*weigths[1]) + (cpVal*weigths[2]) + (m50Val*weigths[3]) )
         
-        createBusynessIndex(request, busynessIndex)
+        #createBusynessIndex(request, busynessIndex)
         
         ans = "Noise, M50, Bikes, CPs:" + str(noiseVal) + ", " + str(m50Val) + ", " + str(bikeVal) + ", " + str(cpVal)
         ans = ans+"<br>"+"Busyness: "+str(busynessIndex)
