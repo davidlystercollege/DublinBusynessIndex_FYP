59b0b33631eb08e656b18895736e0847251c864e
davidlyster <davidlyster@myclick.ie>
2017-04-07 00:42:16 +0000

    trying to fix m50 -2

--- main/views.py
+++ main/views.py
@@ -599,8 +599,8 @@ def mainBusyness(request):
         m50Val = m50times.views.m50times(request)
         createBusynessSub(request, m50_dso, m50Val)
         
-        #cpVal = carParks.views.carParks(request)
-        #createBusynessSub(request, cp_dso, cpVal)
+        cpVal = carParks.views.carParks(request)
+        createBusynessSub(request, cp_dso, cpVal)
         
         #bikeVal = dublinBikes.views.dubBikes(request)
         #createBusynessSub(request, db_dso, bikeVal)
