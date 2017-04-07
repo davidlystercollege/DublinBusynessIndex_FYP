6d0bfee9cd866a013fb897a99e8e8395cd6719ae
davidlyster <davidlyster@myclick.ie>
2017-04-07 12:59:30 +0000

    adding option for full and small dash -1

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.home, name='main home'),
     url(r'^test', views.testing, name='Testing Page'),
     url(r'^dash', views.fulldash, name='Full Dashboard'),
-    url(r'^dashtest', views.testDash, name='Test Dashboard'),
+    url(r'^datasets', views.testDash, name='Test Dashboard'),
     url(r'busyness', views.mainBusyness, name='main busyness output'),
 ]
